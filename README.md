edd1016a179ed983ff2f193341bde98bd68fecde
seblessa <sebastiaolessa2002@outlook.com>
2024-04-29 21:20:19 +0100

    update

---
+++ README.md
@@ -0,0 +1,123 @@
+# Chronos
+
+**Steps to run the code:**
+
+1. Create a virtual environment with python
+`conda create --name myenv python=3.10.13`
+3. Activate in your new virtual environment
+`conda activate myenv`
+4. Install the required requirements
+    - `pip install git+https://github.com/amazon-science/chronos-forecasting.git`
+    - `pip install -r requirements.txt`
+    - 
+if there is a bug with lightbm do this:
+- `brew install libomp`
+And then repeat step 4
+5. Create a folder called `/data` under `Chronos/` and add your data (https://www.kaggle.com/datasets/aslanahmedov/walmart-sales-forecast/data?select=train.csv)
+6. Run the notebook
+
+
+# ZAAI
+Curricular Intership at ZAAI
+
+
+# Summary
+
+The aim of the project is
+to compare the Chronos and TiDE models in the context of time series forecasting. 
+The dataset used is the Walmart Sales Forecasting dataset.
+
+
+TODO:
+
+ZAAI
+
+
+
+A fazer:
+
+
+
+
+RESIDUALS LIMITS: 
+Inicio = 2012-01-06
+
+Test1 = 2012-01-20
+Test2 = 2012-03-30
+Test3 = 2012-06-08
+Test4 = 2012-08-17
+
+Fim = 2012-10-05
+
+
+RELATÓRIO:
+
+O que é transfer learning
+Explicar forecasting
+Definir retail sales
+
+Explicar detalhadamente o dataset utilizado
+
+Explicar os modelos usados (TiDE, Chronos)
+
+Explicar as possibilidades de utilização:
+
+- Simple Chronos (just sales),
+- Simple TiDE (all the covariates),
+- Chronos with the residuals added from the trained TiDE with the dataset created by me based on the original dataset, with the residuals from Chronos.
+
+Concluir a melhor possibilidade de combinação
+
+
+
+
+
+
+
+
+
+
+
+# Versões
+
+The versions of the operating systems used to develop and test this application are:
+- Fedora 38
+- macOS Sonoma 14.0
+
+Python Versions:
+- 3.10.0
+
+
+# Requirements
+
+To keep everything organized and simple,
+we will use [MiniConda](https://docs.conda.io/projects/miniconda/en/latest/) to manage our environments.
+
+To create an environment with the required packages for this project, run the following commands:
+
+```bash
+conda create -n GymEnv python=3.10 pytorch::pytorch torchvision torchaudio -c pytorch
+```
+
+Then we need to install the requirements:
+
+```bash
+pip install -r requirements.txt
+```
+
+# Results
+
+##### The experiments are in this [notebook](notebook.ipynb).
+
+
+
+
+|       TQC in the original environment:        |       TQC in the wrapped environment        |
+|:---------------------------------------------:|:-------------------------------------------:|
+|  ![TQC_original.gif](media/TQC_original.gif)  | ![TQC_wrapped.gif](media%2FTQC_wrapped.gif) |
+
+
+
+|       TRPO in the wrapped environment:        |       PPO in the wrapped environment:       |
+|:---------------------------------------------:|:-------------------------------------------:|
+| ![TRPO_wrapped.gif](media%2FTRPO_wrapped.gif) | ![PPO_wrapped.gif](media%2FPPO_wrapped.gif) |
--- utils.py
+++ utils.py
@@ -1,12 +1,29 @@
-from typing import Tuple
-
-import matplotlib.pyplot as plt
-import matplotlib.ticker as mtick
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import torch
 from chronos import ChronosPipeline
+import matplotlib.ticker as mtick
+import matplotlib.pyplot as plt
+from typing import Tuple
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import torch
+
+
+def evaluation_metrics(prediction: pd.Series, actuals: pd.Series) -> Tuple[float]:
+    predictions = [group for _, group in prediction.groupby('unique_id')]
+    actuals = [group for _, group in actuals.groupby('unique_id')]
+
+    maes = []
+
+    for prediction_df, actuals_df in zip(predictions, actuals):
+        residuals = []
+        for pred, act in zip(prediction_df['forecast'], actuals_df['Weekly_Sales']):
+            residuals.append(act - pred)
+
+        # Calculate Mean Absolute Error (MAE)
+        maes.append(np.mean(np.abs(residuals)))
+
+    # Calculate the final aggregated values
+    return round(np.mean(maes),2)
 
 
 def plot_model_comparison(dataframe: pd.DataFrame) -> None:
@@ -21,7 +38,7 @@ def plot_model_comparison(dataframe: pd.DataFrame) -> None:
     )
     tide_model["model"] = "TiDE"
     tide_model["MAPE"] = (
-        abs(tide_model["Weekly_Sales"] - tide_model["forecast"]) / tide_model["Weekly_Sales"]
+            abs(tide_model["Weekly_Sales"] - tide_model["forecast"]) / tide_model["Weekly_Sales"]
     )
 
     chronos_tiny_model = dataframe.rename(
@@ -29,8 +46,8 @@ def plot_model_comparison(dataframe: pd.DataFrame) -> None:
     )
     chronos_tiny_model["model"] = "Chronos Tiny"
     chronos_tiny_model["MAPE"] = (
-        abs(chronos_tiny_model["Weekly_Sales"] - chronos_tiny_model["forecast"])
-        / chronos_tiny_model["Weekly_Sales"]
+            abs(chronos_tiny_model["Weekly_Sales"] - chronos_tiny_model["forecast"])
+            / chronos_tiny_model["Weekly_Sales"]
     )
 
     chronos_large_model = dataframe.rename(
@@ -38,8 +55,8 @@ def plot_model_comparison(dataframe: pd.DataFrame) -> None:
     )
     chronos_large_model["model"] = "Chronos Large"
     chronos_large_model["MAPE"] = (
-        abs(chronos_large_model["Weekly_Sales"] - chronos_large_model["forecast"])
-        / chronos_large_model["Weekly_Sales"]
+            abs(chronos_large_model["Weekly_Sales"] - chronos_large_model["forecast"])
+            / chronos_large_model["Weekly_Sales"]
     )
 
     plt.rcParams["figure.figsize"] = (20, 5)
@@ -59,7 +76,7 @@ def plot_model_comparison(dataframe: pd.DataFrame) -> None:
 
 
 def chronos_forecast(
-    model: ChronosPipeline, data: pd.DataFrame, horizon: int
+        model: ChronosPipeline, data: pd.DataFrame, horizon: int
 ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
     Generates forecast with Chronos
@@ -81,7 +98,7 @@ def chronos_forecast(
 
 
 def convert_forecast_to_pandas(
-    forecast: list, holdout_set: pd.DataFrame
+        forecast: list, holdout_set: pd.DataFrame
 ) -> pd.DataFrame:
     """
     Convert forecast to pandas data frame
@@ -92,16 +109,63 @@ def convert_forecast_to_pandas(
         pd.DataFrame: forecast in pandas format
     """
 
-    forecast_pd = holdout_set[["unique_id", "Date"]]
-    forecast_pd.loc[:, "forecast_lower"] = forecast[0]
-    forecast_pd.loc[:, "forecast"] = forecast[1]
-    forecast_pd.loc[:, "forecast_upper"] = forecast[2]
+    forecast_pd = holdout_set[["unique_id", "Date"]].copy()
+    forecast_pd["forecast_lower"] = forecast[0]
+    forecast_pd["forecast"] = forecast[1]
+    forecast_pd["forecast_upper"] = forecast[2]
 
     return forecast_pd
 
 
+def plot_multiple_forecasts(
+        actuals_data: pd.DataFrame, forecast_data_list: list, title: str, y_label: str, x_label: str
+) -> None:
+    """
+    Create time series plot of actuals vs multiple forecasts
+    Args:
+        :param actuals_data: actual data from the dataset
+        :param forecast_data_list: list of tuples (forecast dataframe, model name)
+        :param title: title for chart
+        :param x_label
+        :param y_label
+    """
+    # Define a list of colors for each model
+    colors = ['tomato', 'forestgreen', 'royalblue']
+
+    plt.figure(figsize=(20, 5))
+    plt.plot(
+        actuals_data["Date"],
+        actuals_data["Weekly_Sales"],
+        color="black",
+        label="historical data",
+    )
+
+    for i, (forecast_data, model_name) in enumerate(forecast_data_list):
+        plt.plot(
+            forecast_data["Date"],
+            forecast_data["forecast"],
+            color=colors[i],
+            label=model_name + " forecast",
+        )
+        plt.fill_between(
+            forecast_data["Date"],
+            forecast_data["forecast_lower"],
+            forecast_data["forecast_upper"],
+            color=colors[i],
+            alpha=0.3,
+            label=model_name + " 80% prediction interval",
+        )
+
+    plt.ylabel(y_label)
+    plt.xlabel(x_label)
+    plt.title(title)
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+
 def plot_actuals_forecast(
-    actuals_data: pd.DataFrame, forecast_data: pd.DataFrame, title: str
+        actuals_data: pd.DataFrame, forecast_data: pd.DataFrame, title: str
 ) -> None:
     """
     Create time series plot actuals vs forecast
@@ -138,7 +202,7 @@ def plot_actuals_forecast(
     plt.show()
 
 
-def transform_predictions_to_pandas(predictions: list, target: str, pred_list: list, quantiles: list) -> pd.DataFrame:
+def transform_predictions_to_pandas(predictions: list, target: str, pred_list: list, quantiles: list, convert: bool = True) -> pd.DataFrame:
     """
     Receives as list of predictions and transform it in a data frame
     Args:
@@ -147,6 +211,7 @@ def transform_predictions_to_pandas(predictions: list, target: str, pred_list: l
         pred_list (list): list with test df to extract time series id
     Returns
         pd.DataFrame: data frame with date, forecast, forecast_lower, forecast_upper and id
+        :param convert:
     """
 
     pred_df_list = []
@@ -161,12 +226,14 @@ def transform_predictions_to_pandas(predictions: list, target: str, pred_list: l
         temp["forecast_upper"] = p.quantile_df(quantiles[2]).reset_index()[f"{target}_{quantiles[2]}"]
 
         # add unique id
-        temp["unique_id"] = str(int(list(pdf.static_covariates_values())[0][0]))+'-'+str(int(list(pdf.static_covariates_values())[0][1]))
+        temp["unique_id"] = str(int(list(pdf.static_covariates_values())[0][0])) + '-' + str(
+            int(list(pdf.static_covariates_values())[0][1]))
 
-        # convert negative predictions into 0
-        temp[["forecast", "forecast_lower", "forecast_upper"]] = temp[
-            ["forecast", "forecast_lower", "forecast_upper"]
-        ].clip(lower=0)
+        if convert:
+            # convert negative predictions into 0
+            temp[["forecast", "forecast_lower", "forecast_upper"]] = temp[
+                ["forecast", "forecast_lower", "forecast_upper"]
+            ].clip(lower=0)
 
         pred_df_list.append(temp)
 
